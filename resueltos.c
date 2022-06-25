aebb1e5e897be338babe46e6b7c852fc156ca309
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-25 09:01:06 -0500

    add_ok: implementada primera parte de la funcion, sumando positivo con negativo siempre devuelve 1

--- resueltos.c
+++ resueltos.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 float sum_elements(float[],unsigned);
+int add_ok(int,int);
 
 int main(){
     // Prueba de sum_elements
@@ -10,8 +11,35 @@ int main(){
     float lista[5] = {10.2, 10.2, 10.2, 10.2, 10.2};
     float acumulado = sum_elements(lista, 0);
     printf("%f\n",acumulado);
+
+    // Prueba de add_ok
+    // Inicializando x e y
+    // Se imprimira "Operacion valida" si add_ok devuelve 1
+    int x = 0x80000000;
+    int y = 0x7fffffff;
+    if(add_ok(x,y)) printf("Operacion valida\n");
 }
 
+/**
+ * a y b > 0
+ * a > 0
+ * b > 0
+ * a y b < 0
+ */
+int add_ok(int x, int y){
+    if(x<=0){
+        if (y>=0){
+            return 1;
+        }
+    } else if(y<=0){
+        if (x>=0){
+            return 1;
+        }
+    }
+
+    return 0;
+    
+}
 /*
  * sum_elements devuelve la suma de los elementos del array a.
  */

807b2abce9565e7da1533ad312b6cfa44b534201
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-24 21:37:05 -0500

    resueltos.c: prueba del ej01, se copia codigo

---
+++ resueltos.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+float sum_elements(float[],unsigned);
+
+int main(){
+    // Prueba de sum_elements
+    // Inicializando lista[]
+    // La variable acumulado recibira lo retornado por la funcion
+    // Luego se impprime el resultado
+    float lista[5] = {10.2, 10.2, 10.2, 10.2, 10.2};
+    float acumulado = sum_elements(lista, 5);
+    printf("%f\n",acumulado);
+}
+
+/*
+ * sum_elements devuelve la suma de los elementos del array a.
+ */
+float sum_elements(float a[], unsigned length){
+    int i;
+    float result = 0;
+
+    for (i = 0; i <= length; i++){
+        result += a[i];
+    }
+
+    return result;    
+}

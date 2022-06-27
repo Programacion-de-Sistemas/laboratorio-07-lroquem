f21d22eaaf77e22128ce473bb3174fa70027491a
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-27 00:17:49 -0500

    mult_ok: se copio la funcion // mult64_ok: se uso la anterior funcion como base

--- propuestos.c
+++ propuestos.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include<stdint.h>
 
 int bis(int,int);
 int bic(int,int);
 
 int bool_or(int,int);
 int bool_xor(int,int);
+
+int mult_ok(int,int);
+int64_t mult64_ok(int64_t,int64_t);
 
 int main(){
     int x = 175;
@@ -14,9 +18,17 @@ int main(){
 
     x = 684250;
     y = 28457795;
-    if (bool_or(x,y) == (x | y)) printf("APROBADO\n");
-    if (bool_xor(x,y) == (x ^ y)) printf("APROBADO\n");
+    if(bool_or(x,y) == (x | y)) printf("APROBADO\n");
+    if(bool_xor(x,y) == (x ^ y)) printf("APROBADO\n");
+    if(mult_ok(x,y)) printf("APROBADO\n");
 
+    x = 45229;
+    y = 2265;
+    if(mult_ok(x,y)) printf("APROBADO\n");
+
+    int64_t a = 0x35A2FA1744F80BB1;
+    int64_t b = 0x3AE435BD4;
+    if(mult64_ok(a,b)) printf("APROBADO\n");
 }
 
 int bis(int x, int y){
@@ -33,4 +45,14 @@ int bool_or(int x, int y){
 
 int bool_xor(int x, int y){
     return bis(bic(x,y),bic(y,x));
+}
+
+int mult_ok(int x, int y){
+    int p = x * y;
+    return !x || p/x == y;
+}
+
+int64_t mult64_ok(int64_t x, int64_t y){
+    int64_t p = x * y;
+    return !x || p/x == y;
 }
